c435b0c6ee5c16db41a892f41af8399b1feeead9
Zachary Scott <mail@zzak.io>
2017-04-14 12:46:53 +0900

    k8s deployment

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,9 @@ sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/${PROJECT_NAME}
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.config
 
-kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google","image":"asia.gcr.io/'${PROJECT_NAME}'/hello:'"${CIRCLE_SHA1}"'"}]}}}}'
+gcloud container clusters get-credentials cluster-1 --zone asia-east1-a --project zzak-gce
+gcloud config set container/cluster cluster-1
+
+kubectl create -f k8s.yaml --record
+
+kubectl patch deployment hello-deployment -p '{"spec":{"template":{"spec":{"containers":[{"name":"deployment","image":"asia.gcr.io/'${PROJECT_NAME}'/hello:'"${CIRCLE_SHA1}"'"}]}}}}'
