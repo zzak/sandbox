7670f965a84f7bc472cec05715cc6ad5f9852d1d
Zachary Scott <mail@zzak.io>
2017-04-14 11:51:52 +0900

    Try to fix config-helper error

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/hello
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
+sudo chown -R ubuntu:ubuntu /home/ubuntu/.config
 
 kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google","image":"asia.gcr.io/'${PROJECT_NAME}'/hello:'"${CIRCLE_SHA1}"'"}]}}}}'
