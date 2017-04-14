caf8e8f9cef9483bccf2991a6223f90a9ec1cca8
Zachary Scott <mail@zzak.io>
2017-04-14 13:32:30 +0900

    remove chown use full path to bin with sudo

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/hello
-sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-sudo chown -R ubuntu:ubuntu /home/ubuntu/.config
+#sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
+#sudo chown -R ubuntu:ubuntu /home/ubuntu/.config
 
-gcloud container clusters get-credentials cluster-1 --zone asia-east1-a --project zzak-gce
-gcloud config set container/cluster cluster-1
+/opt/google-cloud-sdk/bin/gcloud container clusters get-credentials cluster-1 --zone asia-east1-a --project zzak-gce
+/opt/google-cloud-sdk/bin/gcloud config set container/cluster cluster-1
 
-kubectl create -f k8s.yaml --record --validate=false
+sudo kubectl create -f k8s.yaml --record --validate=false
 
-kubectl patch deployment hello-deployment -p '{"spec":{"template":{"spec":{"containers":[{"name":"deployment","image":"asia.gcr.io/'${PROJECT_NAME}'/hello:'"${CIRCLE_SHA1}"'"}]}}}}'
+sudo kubectl patch deployment hello-deployment -p '{"spec":{"template":{"spec":{"containers":[{"name":"deployment","image":"asia.gcr.io/'${PROJECT_NAME}'/hello:'"${CIRCLE_SHA1}"'"}]}}}}'
