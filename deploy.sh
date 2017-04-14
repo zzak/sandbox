c3d6d74be8de9865ab584728b1d75f4acd6c866a
Zachary Scott <mail@zzak.io>
2017-04-14 13:27:21 +0900

    unexpected args:false

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ sudo chown -R ubuntu:ubuntu /home/ubuntu/.config
 gcloud container clusters get-credentials cluster-1 --zone asia-east1-a --project zzak-gce
 gcloud config set container/cluster cluster-1
 
-kubectl create -f k8s.yaml --record --validate false
+kubectl create -f k8s.yaml --record --validate=false
 
 kubectl patch deployment hello-deployment -p '{"spec":{"template":{"spec":{"containers":[{"name":"deployment","image":"asia.gcr.io/'${PROJECT_NAME}'/hello:'"${CIRCLE_SHA1}"'"}]}}}}'
